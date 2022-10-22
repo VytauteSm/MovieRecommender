432f1e2a397def0abc190d9364689dba83077c5b
LAPTOP-7M3SIRGN\vytau <vytauteskuo@gmail.com>
2022-10-22 11:40:51 +0300

    web with JS

--- MovieRecommender.Web/Pages/IndexWithJs.cshtml.cs
+++ MovieRecommender.Web/Pages/IndexWithJs.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿//do we need this?
+
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MovieRecommender.Web.Pages
@@ -6,6 +8,8 @@ namespace MovieRecommender.Web.Pages
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
+
+        public string SearchType { get; set; } = "T"; //represents search by title
 
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
--- MovieRecommender.Web/wwwroot/js/site.js
+++ MovieRecommender.Web/wwwroot/js/site.js
@@ -1,11 +1,17 @@
-﻿function selectSearchType(searchType) {
+﻿/*do we need this?*/
+
+function selectSearchType(searchType) {
     const form = document.getElementById('mainForm'); //we cant change value of the form
 
     //alert(searchType.value);
 
     const searchTypeSelection = document.getElementById('searchTypeSelected');
 
-    searchTypeSelection.value = "C";
+    searchTypeSelection.value = "C"; //we do it to control what's happening in the server
 
     form.submit(); //data will go to server
+}
+
+function changeSearchForm(searchType) {
+    console.log(searchType.value);
 }
